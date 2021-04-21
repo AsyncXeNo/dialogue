9813c2c196fe19f03f2fd2d5ec4ff46b4acc8709
AsyncXeno <xeno25502@gmail.com>
2021-04-21 14:40:18 +0530

    less suffering

--- dialogue.py
+++ dialogue.py
@@ -1,15 +1,16 @@
 from event import Event
 
 class Dialogue:
-    def __init__(self, name:str, string:str, id:int, options:list = None, event_to_trigger:Event = None):
+    def __init__(self, name:str, string:str, id:int, options:list = None, next:int = None, event_to_trigger:Event = None):
         self.name = name
         self.string = string
         self.id = id
         self.options = options
+        self.next = next
         self.event_to_trigger = event_to_trigger
 
     def __str__(self):
-        return f'{self.name}: {self.string} [Options: {"Yes" if self.options else "No"}]'
+        return f'{self.name}: {self.string} [Options: {"Yes" if self.options else "No"}] [Next: {self.next}]'
 
     def init_options(self):
         self.options = []
--- utils.py
+++ utils.py
@@ -10,6 +10,27 @@ with open('data/options.json') as f:
 	options_object = json.load(f)
 
 
+def get_options(name):
+	options_list = []
+	for option in options_object:
+		if option["name"] == name:
+			for option_for_individual_dialogue in option["options"]:
+				option_id = option_for_individual_dialogue["id"]
+				choices = []
+				for choice in option_for_individual_dialogue["choices"]:
+					choices.append(Option(choice["string"], choice["dialogue"]))
+
+				options_list.append({
+					"id": option_id,
+					"list": choices
+				})
+
+	return options_list
+
+
+print(get_options("person1"))
+
+
 def get_dialogue(name):
 	list_of_dialogues = []
 	for dialogue in dialogues_object:
@@ -19,21 +40,20 @@ def get_dialogue(name):
 				name = individual_dialogue["name"]
 				string = individual_dialogue["string"]
 				options = individual_dialogue["options"]
+				next_dialogue = individual_dialogue["next"]
+
 				options_list = None
 
 				if options:
-					for option in options_object:
-						if option["name"] == name:
-							for options_of_individual_dialogue in option["options"]:
-								if options_of_individual_dialogue["id"] == options:
-									options_list = []
-									for choice in options_of_individual_dialogue["choices"]:
-										options_list.append(Option(choice["string"], choice["dialogue"]))
+					for option in get_options("person1"):
+						if option["id"] == options:
+							options_list = option["list"]
 
-
+				if options_list:
+					print(f'options given to dialogue with id {individual_dialogue["id"]}')
 				list_of_dialogues.append({
 					"id": individual_dialogue["id"],
-					"dialogue": Dialogue(name, string, options_list)
+					"dialogue": Dialogue(name, string, options_list, next_dialogue)
 				})	
 
 	return list_of_dialogues
